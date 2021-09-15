69ae95b3834b915761870382e1bde85136528508
Wilson Lopez <wilson242.wl@gmail.com>
2021-09-14 20:02:52 -0400

    Examen2

---
+++ Crud.Web/SP_14.09.21.Web/SP_14.09.21.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SP_14._09._21.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
